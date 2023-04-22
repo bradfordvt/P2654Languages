6ced485ac89a84f21fe4970c95a0454c0f4aaae9
bradfordvt <bradvt59@gmail.com>
2023-04-22 11:25:15 -0400

    Added new test cases for specific grammar rules.

--- STAM/test/stam_test.py
+++ STAM/test/stam_test.py
@@ -1,12 +1,12 @@
 import sys
 import unittest
 from antlr4 import *
-sys.path.append("c:\\Users\\bvt\\VisualStudioCodeProjects\\P2654Languages\\P2654Languages\\STAM")
+# sys.path.append("c:\\Users\\bvt\\VisualStudioCodeProjects\\P2654Languages\\P2654Languages\\STAM")
 # print(sys.path)
-from dist.STAMLexer import STAMLexer
-from dist.STAMParser import STAMParser
-from dist.STAMVisitor import STAMVisitor
-from MyVisitor import *
+from STAM.dist.STAMLexer import STAMLexer
+from STAM.dist.STAMParser import STAMParser
+from STAM.dist.STAMVisitor import STAMVisitor
+from STAM.MyVisitor import *
 
 
 class MyTestCase(unittest.TestCase):
@@ -16,6 +16,59 @@ class MyTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_namespace_name001(self):
+        data = InputStream('MyNamespace')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.nameSpace_name()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitNameSpace_name(ctx)
+        self.assertEqual(output, 'MyNamespace')
+
+    def test_parameter_ref001(self):
+        data = InputStream('$Param1')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.parameter_ref()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitParameter_ref(ctx)
+        self.assertEqual(output, '$Param1')
+
+    def test_parameter_ref002(self):
+        data = InputStream('$Param1')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.parameter_ref()
+        # evaluator
+        visitor = MyVisitor()
+        visitor.symbols['Param1'] = "MyParameter"
+        output = visitor.visitParameter_ref(ctx)
+        self.assertEqual(output, "MyParameter")
+
+    def test_parameter_name001(self):
+        data = InputStream('Param1')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.parameter_name()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitParameter_name(ctx)
+        self.assertEqual(output, 'Param1')
+
     def test_pos_int001(self):
         data = InputStream('23456')
         # lexer
@@ -23,12 +76,11 @@ class MyTestCase(unittest.TestCase):
         stream = CommonTokenStream(lexer)
         # parser
         parser = STAMParser(stream)
-        tree = parser.pos_int()
+        ctx = parser.pos_int()
         # evaluator
         visitor = MyVisitor()
-        output = visitor.visit(tree)
-        print(output)
-        self.AssertEquals(output, 23456)
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 23456)
 
     def test_pos_int002(self):
         data = InputStream('23_456')
@@ -37,12 +89,11 @@ class MyTestCase(unittest.TestCase):
         stream = CommonTokenStream(lexer)
         # parser
         parser = STAMParser(stream)
-        tree = parser.pos_int()
+        ctx = parser.pos_int()
         # evaluator
         visitor = MyVisitor()
-        output = visitor.visit(tree)
-        print(output)
-        self.AssertEquals(output, 23456)
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 23456)
 
     def test_pos_int003(self):
         data = InputStream('1')
@@ -51,12 +102,11 @@ class MyTestCase(unittest.TestCase):
         stream = CommonTokenStream(lexer)
         # parser
         parser = STAMParser(stream)
-        tree = parser.pos_int()
+        ctx = parser.pos_int()
         # evaluator
         visitor = MyVisitor()
-        output = visitor.visit(tree)
-        print(output)
-        self.AssertEquals(output, 1)
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 1)
 
     def test_pos_int004(self):
         data = InputStream('0')
@@ -65,25 +115,128 @@ class MyTestCase(unittest.TestCase):
         stream = CommonTokenStream(lexer)
         # parser
         parser = STAMParser(stream)
-        tree = parser.pos_int()
+        ctx = parser.pos_int()
         # evaluator
         visitor = MyVisitor()
-        output = visitor.visit(tree)
-        print(output)
-        self.AssertEquals(output, 0)
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 0)
 
-    def test_Attribute001(self):
-        data = InputStream('Attribute atrib1 = "This is the attribute value!";')
+    def test_pos_int005(self):
+        data = InputStream('2')
         # lexer
         lexer = STAMLexer(data)
         stream = CommonTokenStream(lexer)
         # parser
         parser = STAMParser(stream)
-        tree = parser.attribute_def()
+        ctx = parser.pos_int()
         # evaluator
         visitor = MyVisitor()
-        output = visitor.visit(tree)
-        print(output)
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 2)
+
+    def test_pos_int006(self):
+        data = InputStream('3')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.pos_int()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 3)
+
+    def test_pos_int007(self):
+        data = InputStream('4')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.pos_int()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 4)
+
+    def test_pos_int008(self):
+        data = InputStream('5')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.pos_int()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 5)
+
+    def test_pos_int009(self):
+        data = InputStream('6')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.pos_int()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 6)
+
+    def test_pos_int010(self):
+        data = InputStream('7')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.pos_int()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 7)
+
+    def test_pos_int011(self):
+        data = InputStream('8')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.pos_int()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 8)
+
+    def test_pos_int012(self):
+        data = InputStream('9')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.pos_int()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitPos_int(ctx)
+        self.assertEqual(output, 9)
+
+    def test_Attribute001(self):
+        data = InputStream('Attribute atrib1 = "This is the attribute value!", $Parm1, "More value!!!!";')
+        # lexer
+        lexer = STAMLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = STAMParser(stream)
+        ctx = parser.attribute_def()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visitAttribute_def(ctx)
+        self.assertEqual(output, 'atrib1: [ "This is the attribute value!", $Parm1, "More value!!!!" ]')
 
 
 if __name__ == '__main__':
